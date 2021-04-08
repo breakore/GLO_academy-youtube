ca56ab473912617b06532f856c0febbab92272e0
breakore <66332047+breakore@users.noreply.github.com>
2021-04-08 20:52:26 +0300

    Добавлены слайдеры

---
+++ js/slider2.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+
+  },
+
+
+});
