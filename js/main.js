3206468f28c0c90c5d04f75d08daa20b129c4439
breakore <66332047+breakore@users.noreply.github.com>
2021-04-09 18:20:42 +0300

    Добавил правила для адаптива.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,87 @@
-const swiper = new Swiper('.channel-slider', {
+const channelSlider = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
-
   },
-
-
 });
+const recommendedSlider = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
+
+const foodSlider = new Swiper('.food-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.food-button-next',
+      prevEl: '.food-button-prev',
+    },
+  });
+
+const searchButton = document.querySelector('mobile-search');
+const mobileSearch = document.querySelector('input-group');
+searchButton.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open')
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  channelSlider.destroy();
+  recommendedSlider.destroy();
+  foodSlider.destroy();
+}
